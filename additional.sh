5da1acd145259076534b4a7bd2822d1aa579285d
Peter Pfläging <peter@pflaeging.net>
2018-04-18 15:31:30 +0200

    Additional hook for apps!

---
+++ additional.sh
@@ -0,0 +1,7 @@
+#! /bin/sh
+#
+# Put your pre-start code here:
+# This could be for example a copy job to: /container/export/static
+#   because this is mounted, ...
+#
+exit 0
--- runner.sh
+++ runner.sh
@@ -2,5 +2,8 @@
 
 /container/replacer.sh
 
+chmod 755 /container/additional.sh
+/container/additional.sh
+
 python3 manage.py migrate
 python3 manage.py runserver 0.0.0.0:8000
